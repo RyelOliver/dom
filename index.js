3c682105251e12ba981908365c9fd3980101443c
Ryel Oliver <rmuchunku@gmail.com>
2019-07-11 00:03:37 +0100

    Parse index selectors

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 function parsePath(path) {
     return path
+        // Remove trailing space from '>'
         .replace(/>\s+/g, '>')
+        // Add leading space to '>'
         .replace(/(\S)>/g, (match, nonWhiteSpaceCharacter) => `${nonWhiteSpaceCharacter} >`)
+        // Remove leading and spaces within '(n)'
+        .replace(/\s*\(\s*(\S*)\s*\)/g, (match, nonWhiteSpaceCharacter) => `(${nonWhiteSpaceCharacter})`)
+        // Add trailing space to '(n)'
+        .replace(/(\(\S*\))(\S)/g, (match, indexSelector, nonWhiteSpaceCharacter) => `${indexSelector} ${nonWhiteSpaceCharacter}`)
         .split(' ')
+        // Remove any whitespace
         .filter(Boolean)
         .map(selector => {
-            if (selector.indexOf('>') >= 0) {
-                if (selector.length === 1)
-                    throw Error('\'>\' selectors require a following node name');
+            let nodeName = selector;
+            let isDirectChild = false;
+            let index;
 
-                return {
-                    nodeName: selector.substring(1),
-                    isDirectChild: true,
-                };
-            } else {
-                return {
-                    nodeName: selector,
-                };
+            if (nodeName.indexOf('>') >= 0) {
+                nodeName = nodeName.substring(1);
+                isDirectChild = true;
+
+                if (nodeName.length === 0)
+                    throw Error('\'>\' selectors require a following node name');
             }
+
+            if (/(\(.+\))/.test(nodeName)) {
+                const match = nodeName.match(/(\S+)\((\d+)\)/);
+
+                if (!match)
+                    throw Error('\'(n)\' selectors require a preceding node name, e.g. w:t(0)');
+
+                nodeName = match[1];
+                index = parseInt(match[2]);
+            }
+
+            return { nodeName, isDirectChild, index };
         });
 }
 
--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const { parsePath } = require('./index');
 describe('Path parser', () => {
     it('Should parse a single node name selector', () => {
         const expected = [
-            { nodeName: 'w:t' },
+            { nodeName: 'w:t', isDirectChild: false, index: undefined },
         ];
         const actual = parsePath('w:t');
         expect(actual).toStrictEqual(expected);
@@ -11,8 +11,8 @@ describe('Path parser', () => {
 
     it('Should parse multiple node name selectors', () => {
         const expected = [
-            { nodeName: 'w:r' },
-            { nodeName: 'w:t' },
+            { nodeName: 'w:r', isDirectChild: false, index: undefined },
+            { nodeName: 'w:t', isDirectChild: false, index: undefined },
         ];
         const actual = parsePath('w:r w:t');
         expect(actual).toStrictEqual(expected);
@@ -20,8 +20,8 @@ describe('Path parser', () => {
 
     it('Should parse multiple node name selectors with varying spaces', () => {
         const expected = [
-            { nodeName: 'w:r' },
-            { nodeName: 'w:t' },
+            { nodeName: 'w:r', isDirectChild: false, index: undefined },
+            { nodeName: 'w:t', isDirectChild: false, index: undefined },
         ];
         expect(parsePath('w:r  w:t')).toStrictEqual(expected);
         expect(parsePath(' w:r w:t')).toStrictEqual(expected);
@@ -31,7 +31,7 @@ describe('Path parser', () => {
 
     it('Should parse a direct child node selector', () => {
         const expected = [
-            { nodeName: 'w:t', isDirectChild: true },
+            { nodeName: 'w:t', isDirectChild: true, index: undefined },
         ];
         expect(parsePath('>w:t')).toStrictEqual(expected);
         expect(parsePath('> w:t')).toStrictEqual(expected);
@@ -39,5 +39,19 @@ describe('Path parser', () => {
 
     it('Should error parsing a direct child node selector without a following node name', () => {
         expect(() => parsePath('>')).toThrowError('\'>\' selectors require a following node name');
+    });
+
+    it('Should parse index selectors', () => {
+        const expected = [
+            { nodeName: 'w:r', isDirectChild: false, index: 0 },
+            { nodeName: 'w:t', isDirectChild: false, index: 12 },
+        ];
+        expect(parsePath('w:r(0) w:t(12)')).toStrictEqual(expected);
+        expect(parsePath('w:r (0)   w:t( 12 )')).toStrictEqual(expected);
+    });
+
+    it('Should error parsing an index selector without a preceding node name or without an integer', () => {
+        expect(() => parsePath('(40)')).toThrowError('\'(n)\' selectors require a preceding node name');
+        expect(() => parsePath('w:t(n)')).toThrowError('\'(n)\' selectors require a preceding node name');
     });
 });
